c4bba1b7ef63ba9a103a130bcb8573758dd09f95
Oumimoun <khalil.oumimoun@gmail.com>
2020-04-20 20:09:08 -0500

    terrain and camera

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    public Transform target;
+    public Vector3 target_Offset;
+
+    private void Start()
+    {
+        target_Offset = transform.position - target.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target)
+        {
+            transform.position = Vector3.Lerp(transform.position, target.position+target_Offset, 0.1f);
+        }
+   
+    }
+}
